1bc6b1fb67ff9ada0c6e1a2db33654439f273f9f
Srija Vuppala <srijavuppala11@gmail.com>
2025-07-31 17:16:35 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n", "") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to End ML Project',
+    author='Srija V',
+    install_requires=get_requirements('requirements.txt')
+)
